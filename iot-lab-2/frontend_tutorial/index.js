5ea5534071794f0155726aa7fcabe687708aa200
Dhruv Rajgharia <dhruvr4@illinois.edu>
2022-10-05 17:52:55 -0500

    done

--- iot-lab-2/frontend_tutorial/index.js
+++ iot-lab-2/frontend_tutorial/index.js
@@ -1,5 +1,5 @@
-var server_port = 65432;
-var server_addr = "192.168.3.49";   // the IP address of your Raspberry PI
+var server_port = 8080;
+var server_addr = "192.168.3.113" ;   // the IP address of your Raspberry PI
 
 function client(){
     
