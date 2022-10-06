2970964185b61aecbb6cac9d039424c87035b7ba
Dhruv Rajgharia <dhruvr4@illinois.edu>
2022-10-05 19:23:23 -0500

    done

--- iot-lab-2/frontend_tutorial/wifi_server.py
+++ iot-lab-2/frontend_tutorial/wifi_server.py
@@ -3,6 +3,23 @@ import socket
 HOST = "192.168.3.113" # IP address of your Raspberry PI
 PORT = 8080          # Port to listen on (non-privileged ports are > 1023)
 import picar_4wd
+
+def move(s):
+    power = 20
+    if(s == "87"):
+        picar_4wd.forward(power)
+    elif(s == "83"):
+        picar_4wd.backward(power)
+    elif(s == "65"):
+        picar_4wd.turn_left(power)
+    elif(s == "68"):
+        picar_4wd.turn_right(power)
+    else:
+        picar_4wd.stop()
+            
+
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -12,7 +29,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             client, clientInfo = s.accept()
             print("server recv from: ", clientInfo)
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
-            #picar_4wd.forward(20)
             if data != b"":
                 s = data.decode('ascii')
                 print(s)
