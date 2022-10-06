e051f23a14e9a602f8016add23a866b1fb3bd06f
Dhruv Rajgharia <dhruvr4@illinois.edu>
2022-10-05 19:08:48 -0500

    done

--- iot-lab-2/frontend_tutorial/wifi_server.py
+++ iot-lab-2/frontend_tutorial/wifi_server.py
@@ -13,6 +13,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             print("server recv from: ", clientInfo)
             data = client.recv(1024)      # receive 1024 Bytes of message in binary format
             if data != b"":
+                if(data.includes("87")):
+                    print("he")
                 if(data == "b\'87\\r\\n\'"):
                     print("hi")
                     picar_4wd.forward(30)
