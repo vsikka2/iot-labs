1661679a71fee9bf5d36c75f0f26e6a503036805
Dhruv Rajgharia <dhruvr4@illinois.edu>
2022-10-08 18:20:43 -0500

    some work

--- iot-lab-2/frontend_tutorial/bt_client.py
+++ iot-lab-2/frontend_tutorial/bt_client.py
@@ -8,6 +8,7 @@ while 1:
     text = input("Enter your message: ") # Note change to the old (Python 2) raw_input
     if text == "quit":
         break
+    text = bytes(text,'UTF-8')
     sock.send(text)
 
     data = sock.recv(1024)
